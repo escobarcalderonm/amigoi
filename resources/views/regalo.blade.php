045e3bb68b058c87da5e8068719bfd2f92baf78b
Maikel Escobar <mayk.698@gmail.com>
2018-12-02 21:59:43 +0100

    update amigoi

--- resources/views/regalo.blade.php
+++ resources/views/regalo.blade.php
@@ -36,25 +36,25 @@
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
                     <ul class="nav navbar-nav menu_nav ml-auto">
-                        <li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="about-us.html">About</a></li>
-                        <li class="nav-item"><a class="nav-link" href="services.html">Services</a>
-                        <li class="nav-item submenu dropdown">
-                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pages</a>
-                            <ul class="dropdown-menu">
-                                <li class="nav-item"><a class="nav-link" href="portfolio.html">Portfolio</a></li>
-                                <li class="nav-item"><a class="nav-link" href="portfolio-details.html">Project Details</a></li>
-                                <li class="nav-item"><a class="nav-link" href="elements.html">Elements</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item submenu dropdown">
-                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Blog</a>
-                            <ul class="dropdown-menu">
-                                <li class="nav-item"><a class="nav-link" href="blog.html">Blog</a></li>
-                                <li class="nav-item"><a class="nav-link" href="single-blog.html">Blog Details</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+                        {{--<li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li>--}}
+                        {{--<li class="nav-item"><a class="nav-link" href="about-us.html">About</a></li>--}}
+                        {{--<li class="nav-item"><a class="nav-link" href="services.html">Services</a>--}}
+                        {{--<li class="nav-item submenu dropdown">--}}
+                            {{--<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pages</a>--}}
+                            {{--<ul class="dropdown-menu">--}}
+                                {{--<li class="nav-item"><a class="nav-link" href="portfolio.html">Portfolio</a></li>--}}
+                                {{--<li class="nav-item"><a class="nav-link" href="portfolio-details.html">Project Details</a></li>--}}
+                                {{--<li class="nav-item"><a class="nav-link" href="elements.html">Elements</a></li>--}}
+                            {{--</ul>--}}
+                        {{--</li>--}}
+                        {{--<li class="nav-item submenu dropdown">--}}
+                            {{--<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Blog</a>--}}
+                            {{--<ul class="dropdown-menu">--}}
+                                {{--<li class="nav-item"><a class="nav-link" href="blog.html">Blog</a></li>--}}
+                                {{--<li class="nav-item"><a class="nav-link" href="single-blog.html">Blog Details</a></li>--}}
+                            {{--</ul>--}}
+                        {{--</li>--}}
+                        {{--<li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>--}}
                     </ul>
                 </div>
             </div>
@@ -237,7 +237,7 @@
                     <div class="projects_item">
                         <img class="img-fluid" src="{{asset($pare->foto)}}" alt="">
                         <div class="projects_text">
-                            <h4>{{$pare->nombre}}</h4>
+                            <h4 style="color: white">{{$pare->nombre}}</h4>
                             {{--<p>Client Project</p>--}}
                         </div>
                     </div>
