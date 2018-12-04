0a25ca7bf55a73eda1951f0e69d4cb412abeff53
Maikel Escobar <mayk.698@gmail.com>
2018-12-04 17:54:17 +0100

    update amigoi

--- public/php/notifications.php
+++ public/php/notifications.php
@@ -5,24 +5,28 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     foreach ($_POST as $key => $value) {
         $data .= "Field [".htmlspecialchars($key)."] is [".htmlspecialchars($value)."]\n";
     }
-    $myfile = fopen("newfile".uniqid().".txt", "w") or die("Unable to open file!");
-    $txt = "Comienzo de la peticion\n";
-    fwrite($myfile, $txt);
-    $txt = $data;
-    fwrite($myfile, $txt);
-    fclose($myfile);
+    $fichero = 'file.txt';
+// Abre el fichero para obtener el contenido existente
+    $actual = file_get_contents($fichero);
+// Añade una nueva persona al fichero
+    $actual .= date('l jS \of F Y h:i:s A')."\n";
+    $actual .= $data."\n";
+// Escribe el contenido al fichero
+    file_put_contents($fichero, $actual);
     echo 'Todo recibido';
 }else{
     $data = '';
     foreach ($_GET as $key => $value) {
         $data .= "Field [".htmlspecialchars($key)."] is [".htmlspecialchars($value)."]\n";
     }
-    $myfile = fopen("newfile".uniqid().".txt", "w") or die("Unable to open file!");
-    $txt = "NO ES POST\n";
-    fwrite($myfile, $txt);
-    $txt = $data;
-    fwrite($myfile, $txt);
-    fclose($myfile);
+    $fichero = 'file.txt';
+// Abre el fichero para obtener el contenido existente
+    $actual = file_get_contents($fichero);
+// Añade una nueva persona al fichero
+    $actual .= date('l jS \of F Y h:i:s A')."\n";
+    $actual .= $data."\n";
+// Escribe el contenido al fichero
+    file_put_contents($fichero, $actual);
     echo 'No es post';
 }
 
