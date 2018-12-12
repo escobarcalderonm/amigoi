f53d84b99032428297ecbbbe41499fb427537823
Maikel Escobar <mayk.698@gmail.com>
2018-12-12 18:11:49 +0100

    update amigoi

--- resources/views/twitter.blade.php
+++ resources/views/twitter.blade.php
@@ -3,10 +3,10 @@
 use Abraham\TwitterOAuth\TwitterOAuth;
 
 $settings = array(
-    'oauth_access_token' => "1067796569527894017-j2tQbR5rTwVWD0h7GfWKq6S4PCBDJC",
-    'oauth_access_token_secret' => "3IBIharSvxe6663t7hp3Sjz966J3DLXnEHsbLIjvsT19s",
-    'consumer_key' => "	F8gWymDTNrYcA3SY8JIFKPmL1",
-    'consumer_secret' => "Rp5f0WJNOgtQd5L63uWqWvOa3GihaqpZ4F6efhVK6lqDQnqIjj"
+    'oauth_access_token' => "1067796569527894017-tHCNSVCIRYY2W7Qu04hzs0u2vFRmGY",
+    'oauth_access_token_secret' => "dm2Su5Sn72m0mVXFfRHUDFiIAZ1MsmbVG8uDXtE1BpW0C",
+    'consumer_key' => "	Gdvij9eg9W2Ql8UGcWWvsj8hk",
+    'consumer_secret' => "gch3o0EAljlhzbEOOL9yYLlg3kYuPzgdzecohFUqSn1TUiIgwo"
 );
 
 $connection = new TwitterOAuth($settings['consumer_key'], $settings['consumer_secret'], $settings['oauth_access_token'], $settings['oauth_access_token_secret']);
